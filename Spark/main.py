115c25513fec6260b2bc71373118be1750cfa5cb
chalkley <chalkley@ohsu.edu>
2019-04-15 14:23:23 -0700

    filter all success! part 2 commence!

--- Spark/main.py
+++ Spark/main.py
@@ -25,7 +25,7 @@ def main():
 
     sc = spark.sparkContext
 
-    reloadFiles=True
+    reloadFiles=False
     badMonths=[(12,17),(6,12)]
     #add list of poorly nehaving files, to include 2012-06
     if reloadFiles:
@@ -53,7 +53,7 @@ def main():
     else: 
         filtered=sc.read.parquet('filtered_all.parquet')
     
-    part2=False
+    part2=True
     if part2:
         file="/mnt/filevault-b/2/homes/chalkley/cluster/RedditProject/Spark/wordCollections.dic"
         output='collection_frequencies'
