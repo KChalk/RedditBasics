8070c8e0a9bbd0f844d0112ac509d6934effec5e
chalkley <chalkley@ohsu.edu>
2019-04-19 18:37:49 -0700

    a mess that wont work

--- Spark/main.py
+++ Spark/main.py
@@ -46,7 +46,8 @@ def main():
         print('\n\n\n starting read and filter')
         filteredDFs=fRDD.map(lambda x: Row(fname=x[0], filteredDF=filterPosts(x[0],sc,spark,subs=set(sub_list))))
         df=createDataFrame(filteredDFs)
-        df.write.parquet('filtered_all.parquet', mode='overwrite')
+        allmonths=df.filteredDF.union(df.filteredDF)
+        allmonths.write.parquet('filtered_all.parquet', mode='overwrite')
 
     else: 
         filtered=spark.read.parquet('filtered_all.parquet')
